841ba35f9a75134a90ebc31190fb33e3701c4c64
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2024-08-22 14:34:52 -0300

    wip

--- src/app.js
+++ src/app.js
@@ -493,8 +493,8 @@ class StateMachine {
                 credorInfo.length +
                 "."
             );
-            await this._handleMenuState(origin, phoneNumber, response);
-            this._setCurrentState(phoneNumber, "MENU");
+            await this._handleCredorState(origin, phoneNumber, response);
+            this._setCurrentState(phoneNumber, "CREDOR");
             return;
           }
 
