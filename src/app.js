c7cf6de06b46ef550685d0a74a4b9330de790f8a
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2024-12-09 14:19:59 -0300

    alterado tamanho base64

--- src/app.js
+++ src/app.js
@@ -40,9 +40,9 @@ const customDbConfig = {
   connectTimeout: 60000,
 };
 
-app.use(express.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("qrcodes"));
 
 if (!fs.existsSync(qrCodeDataPath)) {
