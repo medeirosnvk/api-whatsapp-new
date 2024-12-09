85a173a9334f8aafc9d4fbaf7e352df21195d180
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2024-12-09 16:42:27 -0300

    wip

--- src/services/sessionsManager.js
+++ src/services/sessionsManager.js
@@ -12,20 +12,10 @@ module.exports = {
     return sessions.get(sessionName);
   },
   getAllSessions: () => {
-    try {
-      if (sessions.size === 0) {
-        return []; // Retorna um array vazio se não houver sessões
-      }
-
-      return Array.from(sessions.entries()).map(([sessionName, sessionData]) => ({
-        sessionName,
-        connectionState: sessionData.connectionState || "unknown", // Valor padrão
-        ...sessionData,
-      }));
-    } catch (error) {
-      console.error("Error retrieving all sessions:", error);
-      throw new Error("Erro ao buscar sessões.");
-    }
+    return Array.from(sessions.entries()).map(([sessionName, sessionData]) => ({
+      sessionName,
+      ...sessionData,
+    }));
   },
   updateSession: (sessionName, updates) => {
     const session = sessions.get(sessionName);
