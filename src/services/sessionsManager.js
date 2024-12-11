ef563a34367571a51ff35e314c98e2843bc465ee
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2024-12-11 10:46:42 -0300

    clientInfo

--- src/services/sessionsManager.js
+++ src/services/sessionsManager.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 const sessions = new Map(); // Armazena as informações das sessões
 
@@ -9,19 +10,33 @@ module.exports = {
       ...additionalData,
     });
   },
+
   getSession: (sessionName) => {
     return sessions.get(sessionName);
   },
+
   getAllSessions: () => {
     return Array.from(sessions.entries()).map(([sessionName, sessionData]) => {
       const { client, ...safeData } = sessionData;
 
-      // Exibe o conteúdo de `client` no console
+      // Verifica se o client e info estão presentes
+      const clientInfo = client?.info
+        ? {
+            pushname: client.info.pushname,
+            wid: client.info.wid,
+            me: client.info.me,
+            phone: client.info.phone,
+            platform: client.info.platform,
+          }
+        : null;
+
+      // Exibe o conteúdo de client no console
       console.log(`Session "${sessionName}" client data:`, client);
 
       return {
         sessionName,
-        connectionState: safeData.connectionState, // Apenas retorna sessionName e connectionState
+        connectionState: safeData.connectionState, // Retorna sessionName e connectionState
+        info: clientInfo, // Inclui os dados de info, se existirem
       };
     });
   },
@@ -36,6 +51,7 @@ module.exports = {
       ...updates, // Atualiza os dados com as novas informações
     });
   },
+
   removeSession: (sessionName) => {
     sessions.delete(sessionName);
   },
