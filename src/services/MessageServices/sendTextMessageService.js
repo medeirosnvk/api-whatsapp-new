467f94a23a4dfa07bb3486065de9429e00d1acdc
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2025-04-08 19:38:05 -0300

    wip

--- src/services/MessageServices/sendTextMessageService.js
+++ src/services/MessageServices/sendTextMessageService.js
@@ -11,28 +11,30 @@ const sendTextMessage = async (sessionName, phoneNumber, message) => {
     throw new Error(`Sessão ${sessionName} não está conectada. Estado atual: ${session.connectionState}`);
   }
 
-  let processedNumber = phoneNumber;
-  const brazilCountryCode = "55";
+  // Formatar número
+  const formattedNumber = phoneNumber.replace(/\D/g, ""); // Remove tudo que não for número
 
-  if (processedNumber.startsWith(brazilCountryCode)) {
-    const localNumber = processedNumber.slice(4);
-
-    if (localNumber.length === 9 && localNumber.startsWith("9")) {
-      processedNumber = brazilCountryCode + processedNumber.slice(2, 4) + localNumber.slice(1);
-    }
+  if (!formattedNumber.startsWith("55") || formattedNumber.length < 12 || formattedNumber.length > 13) {
+    throw new Error(`Número ${phoneNumber} inválido. Deve estar no formato internacional começando com 55.`);
   }
 
-  console.log(`Número processado: ${processedNumber}`);
+  const jid = `${formattedNumber}@c.us`;
+
+  // Verificar se está registrado no WhatsApp
+  const isRegistered = await session.client.isRegisteredUser(jid);
+  if (!isRegistered) {
+    throw new Error(`O número ${formattedNumber} não está registrado no WhatsApp.`);
+  }
+
+  console.log(`Número processado: ${formattedNumber}`);
   console.log(`Texto: ${message.text}`);
 
   try {
-    await session.client.sendMessage(`${processedNumber}@c.us`, message.text);
+    await session.client.sendMessage(jid, message.text);
     console.log(`Mensagem enviada para ${phoneNumber} na sessão ${sessionName}: ${message.text}`);
   } catch (error) {
-    console.error(`- Erro: ${error.message}`);
-    console.error(`- Stack: ${error.stack}`);
-    console.error(`- Detalhes adicionais:`, error);
-    throw new Error(`Erro ao tentar enviar sendTextMessage: ${error.message}`);
+    console.error(`Erro ao enviar mensagem: ${error.message}`);
+    throw new Error(`Erro ao tentar enviar mensagem: ${error.message}`);
   }
 };
 
