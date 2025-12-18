d4f83d66ebbc6a17d023b1f2508b5226b33103df
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2025-12-18 15:12:48 -0300

    testar erro Lid

--- src/services/MessageServices/sendTextMessageService.js
+++ src/services/MessageServices/sendTextMessageService.js
@@ -3,7 +3,7 @@ const sessionManager = require("../../services/sessionsManager");
 const sendTextMessage = async (sessionName, phoneNumber, message) => {
   const session = sessionManager.getSession(sessionName);
 
-  if (!session.client) {
+  if (!session?.client) {
     throw new Error(`Sessão ${sessionName} não encontrada.`);
   }
 
@@ -15,23 +15,29 @@ const sendTextMessage = async (sessionName, phoneNumber, message) => {
   const brazilCountryCode = "55";
 
   if (processedNumber.startsWith(brazilCountryCode)) {
+    const ddd = processedNumber.slice(2, 4);
     const localNumber = processedNumber.slice(4);
 
     if (localNumber.length === 9 && localNumber.startsWith("9")) {
-      processedNumber = brazilCountryCode + processedNumber.slice(2, 4) + localNumber.slice(1);
+      processedNumber = brazilCountryCode + ddd + localNumber.slice(1);
     }
   }
 
-  console.log(`Número processado: ${processedNumber}`);
-  console.log(`Texto: ${message.text}`);
+  const jid = `${processedNumber}@c.us`;
 
   try {
-    await session.client.sendMessage(`${processedNumber}@c.us`, message.text);
-    console.log(`Mensagem enviada para ${phoneNumber} na sessão ${sessionName}: ${message.text}`);
+    // Força o WhatsApp a resolver o contato e gerar o LID
+    const contact = await session.client.getContactById(jid);
+
+    if (!contact) {
+      throw new Error("Contato não encontrado ou inválido");
+    }
+
+    await session.client.sendMessage(contact.id._serialized, message.text);
+
+    console.log(`Mensagem enviada para ${processedNumber} na sessão ${sessionName}`);
   } catch (error) {
-    console.error(`- Erro: ${error.message}`);
-    console.error(`- Stack: ${error.stack}`);
-    console.error(`- Detalhes adicionais:`, error);
+    console.error("Erro ao enviar mensagem:", error);
     throw new Error(`Erro ao tentar enviar sendTextMessage: ${error.message}`);
   }
 };
